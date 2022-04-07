e4b35719acba2bc7ce154121550a0ca2a4b35100
Guillermo Santos <santosguillermo360@outlook.com>
2022-04-07 11:25:56 -0400

    Servicio de navegacion entre pantallas

---
+++ App2/App2/Services/IViewService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Care_Taker.Services
+{
+    public interface IViewService
+    {
+        Task PushAsync(ContentPage page);
+        Task<ContentPage> PopAsync();
+        Task<bool> DisplayAlert(string title, string message, string ok, string cancel);
+        Task DisplayAlert(string title, string message, string ok);
+    }
+}
---
+++ App2/App2/Services/ViewService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Care_Taker.Services
+{
+    public class ViewService : IViewService
+    {
+        public async Task DisplayAlert(string title, string message, string ok)
+        {
+            await MainPage.DisplayAlert(title, message, ok);
+        }
+
+        public async Task<bool> DisplayAlert(string title, string message, string ok, string cancel)
+        {
+            return await MainPage.DisplayAlert(title, message, ok, cancel);
+        }
+
+        public async Task PushAsync(ContentPage page)
+        {
+            await MainPage.Navigation.PushAsync(page);
+        }
+
+        public async Task<ContentPage> PopAsync()
+        {
+            return (ContentPage)await MainPage.Navigation.PopAsync();
+        }
+
+        private Page MainPage
+        {
+            get { return Application.Current.MainPage; }
+        }
+    }
+}
